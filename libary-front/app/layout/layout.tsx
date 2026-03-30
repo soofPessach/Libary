920bae629e7ab0b159ddff6a70e04193fee53e8c
soofPessach <soof.pess@gmail.com>
2026-03-30 10:58:41 +0300

    change login

--- libary-front/app/layout/layout.tsx
+++ libary-front/app/layout/layout.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import MainAppBar from "./mainAppBar/mainAppBar";
 import { Theme } from "@radix-ui/themes";
 import { useLoginUserId } from "~/global/zustand/loginUserId";
-import Login from "~/routes/login";
 import { useNavigate } from "react-router";
 
 interface layoutProps {
@@ -19,18 +18,16 @@ function Layout(props: layoutProps) {
 
   return (
     <Theme>
-      {loginUserId ? (
-        <div className="min-h-screen bg-gradient-to-b from-slate-50 via-white to-slate-100 text-slate-900">
-          <header className="sticky top-0  z-40 bg-white/80 backdrop-blur border-b border-slate-200">
+      <div className="min-h-screen bg-gradient-to-b from-slate-50 via-white to-slate-100 text-slate-900">
+        {loginUserId && (
+          <header className="sticky top-0 z-40 bg-white/80 backdrop-blur border-b border-slate-200">
             <div className="mx-auto max-w-6xl px-4">
               <MainAppBar />
             </div>
           </header>
-          <main className="mx-auto max-w-6xl px-4 py-10">{props.children}</main>
-        </div>
-      ) : (
-        <Login />
-      )}
+        )}
+        <main className="mx-auto max-w-6xl px-4 py-10">{props.children}</main>
+      </div>
     </Theme>
   );
 }
